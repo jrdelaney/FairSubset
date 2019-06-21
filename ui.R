1f6cdd0c8a56098648f35d4a676776f074581982
Joe Delaney <31332006+jrdelaney@users.noreply.github.com>
2019-06-21 12:55:44 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,36 +1,54 @@
 library(shiny)
 library(ggplot2)
+library(shinyjs)
+library(shinyBS)
 
 fluidPage(
   
-  titlePanel("Subset your data into equal sample sizes which are best representative of each complete set of data.  Use subsets for plotting purposes."),
+  titlePanel("FairSubset: Subset your data into equal sample sizes which are best representative of each complete set of data"),
   
   sidebarPanel(
     
     tags$head(
-      tags$style(HTML('#run_stats{background-color:#9999FF; text-color:black; border-color:black; font-weight:bold}'))
+      tags$style(HTML('#run_simulations{background-color:#9999FF; text-color:black; border-color:black; font-weight:bold}'))
     ),
-    
-    #tags$style(type="text/css",
-    #           ".shiny-output-error { visibility: hidden; }",
-    #           ".shiny-output-error:before { visibility: hidden; }"
-    #),
-    
+
     textAreaInput('pasted_data_1',"Paste your data here, with each sample in a column", value = "" ,width = "67%", height = "67%", resize = "both"),
     fileInput('uploaded_data_1', 'Or upload your data', multiple = FALSE),
     downloadLink("example_data_download", "Download example input data"),
     br(),
     br(),
+    
     actionButton('run_simulations', 'Find best representative subset'),
-    helpText("Runs 1000 subsets of data to test for averages and standard deviations which are most similar to your original data.  By default, N is your smallest sample size."),
+    actionButton("refresh", "Refresh All Inputs"),
+    
+    conditionalPanel(
+      condition = "input.average_setting != 'Kolmogorov-Smirnov' ",
+    helpText("Runs 1000 subsets of data to test for averages and standard deviations which are most similar to your original data.  By default, N is your smallest sample size.")
+    ),
+    conditionalPanel(
+      condition = "input.average_setting == 'Kolmogorov-Smirnov' ",
+      helpText("Runs 1000 subsets of data to test for data distributions which are most similar to your original data.  By default, N is your smallest sample size.")
+    ),
     textInput('manual_N',"Manually set desired number of data points (N) per group"),
-    selectInput('average_setting', 'Type of average', c("Mean","Median"), selected = "Mean"),
+    selectInput('average_setting', 'Subsetting criterion', c("Mean","Median", "Kolmogorov-Smirnov"), selected = "Mean"),
+    bsTooltip(id = "average_setting", 
+              title =  "Mean or median will choose the subset averages and SDs which best match the original data.  The Kolmogorov-Smirnov option will perform KS tests and select the subset with the highest p-value (most similar to) the original data, and can be used for skewed data.",
+              placement = "top", options = NULL
+    ),
     sliderInput('plot_width', "Plot width", min = 0, max = 1000, value = 400),
     sliderInput('plot_height', "Plot height", min = 0, max = 2000, value = 210)
 
   ),
     
     mainPanel(
+      conditionalPanel(
+        condition = "output.executed != 'done' ",
+        helpText('Please input your data.  For an example of how the data should be formatted, please click "Download example input data"'),
+        br(),
+        img(src='FairSubset_landing.png', align = "left", width = 637, height = 302)
+      ),
+      
       conditionalPanel(
         condition = "output.executed == 'done' ",
       span(textOutput('warning'), style="color:red"),
